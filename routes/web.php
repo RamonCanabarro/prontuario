4ba5bb79d52d1acbe84e270b91e9bd5915ee09e5
Ricardo Caldeira <ricardocaldeiralima@gmail.com>
2017-05-06 03:18:53 -0300

    Exemplos de rotas e pagina de testes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//inicio rotas de exemplo
+
+//Route::get('/deposito/form', ['uses' => 'DepositoController@form', 'as' => 'deposito.form']);
+//Route::get('/deposito', ['uses' => 'DepositoController@index', 'as' => 'deposito.index']);
+//Route::post('/deposito/salvar', ['uses' => 'DepositoController@salvar', 'as' => 'deposito.salvar']);
+//Route::get('/deposito/excluir/{id_deposito}', ['uses' => 'DepositoController@excluir', 'as' => 'deposito.excluir'])->where('id_deposito', '[0-9]+');
+//Route::get('/deposito/alterar/{id_deposito}', ['uses' => 'DepositoController@form', 'as' => 'deposito.alterar'])->where('id_deposito', '[0-9]+');
+//Route::get('/ativar/{id_deposito}', ['uses' => 'DepositoController@ativar', 'as' => 'deposito.ativo']);
+
+Route::get('/teste', ['uses' => 'TesteController@index', 'as' => 'teste.index']);
