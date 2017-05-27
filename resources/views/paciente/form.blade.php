3433f5c7441537ba6c6c5b4ef685f9a6dc51b2af
RamonCanabarro <ramoncanabarro@gmail.com>
2017-05-27 17:58:13 -0300

    Ricardo-exemplo inicial.

--- resources/views/paciente/form.blade.php
+++ resources/views/paciente/form.blade.php
@@ -1,7 +1,74 @@
 @extends('layouts.app')
+<!-- Adicionando Javascript -->
+<script type="text/javascript" >
 
+    function limpa_formulário_cep() {
+        //Limpa valores do formulário de cep.
+        document.getElementById('rua').value=("");
+        document.getElementById('bairro').value=("");
+        document.getElementById('cidade').value=("");
+        document.getElementById('uf').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor;
+//                .replace(/\D/g, '');
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = '//viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+</script>
 @section('conteudo')
-    <form action="#" method="post">
+    <form action="list.php" method="post">
         <div class="panel panel-primary">
             <div class="panel-heading" align="center">
                 <h4>Cadastro de Pacientes</h4>
@@ -28,11 +95,13 @@
                     </div>
                     <label for="nascimento" class="control-label col-md-2">Nascimento:</label>
                     <div class="col-md-10">
-                        <input type="date" placeholder="" id="nascimento" name="nascimento" required class="form-control"/>
+                        <input type="date" placeholder="" id="nascimento" name="nascimento" required
+                               class="form-control"/>
                     </div>
                     <label for="estado" class="control-label col-md-2">Estado civil:</label>
                     <div class="col-md-10">
-                        <input type="text" placeholder="Estado civil" id="estado" name="estado" required class="form-control"/>
+                        <input type="text" placeholder="Estado civil" id="estado" name="estado" required
+                               class="form-control"/>
                     </div>
                 </div>
 
@@ -51,11 +120,32 @@
                     </div>
                     <label for="cep" class="control-label col-md-2">CEP:</label>
                     <div class="col-md-10">
-                        <input type="number" placeholder="Cep" id="cep" name="cep" required class="form-control"/>
+                        <input name="cep" type="text" id="cep" value="" size="10" maxlength="9" class="form-control" onblur="pesquisacep(this.value);"/>
                     </div>
-                    <label for="endereco" class="control-label col-md-2">Endereço:</label>
+                    <label for="rua" class="control-label col-md-2">Endereço:</label>
                     <div class="col-md-10">
-                        <input type="text" placeholder="Endereço" id="endereco" name="endereco" required class="form-control"/>
+                        <input type="text" placeholder="rua" id="rua" name="rua" required
+                               class="form-control"/>
+                    </div>
+                    <label for="numero" class="control-label col-md-2">Numero:</label>
+                    <div class="col-md-10">
+                        <input type="text" placeholder="numero" id="numero" name="numero" required
+                               class="form-control"/>
+                    </div>
+                    <label for="bairro" class="control-label col-md-2">Bairro:</label>
+                    <div class="col-md-10">
+                        <input type="text" placeholder="bairro" id="bairro" name="bairro" required
+                               class="form-control"/>
+                    </div>
+                    <label for="cidade" class="control-label col-md-2">Cidade:</label>
+                    <div class="col-md-10">
+                        <input type="text" placeholder="cidade" id="cidade" name="cidade" required
+                               class="form-control"/>
+                    </div>
+                    <label for="uf" class="control-label col-md-2">UF:</label>
+                    <div class="col-md-10">
+                        <input type="text" placeholder="uf" id="uf" name="uf" required
+                               class="form-control"/>
                     </div>
                     <label for="raca" class="control-label col-md-2">Raça:</label>
                     <div class="col-md-10">
@@ -68,8 +158,9 @@
             </div>
 
             <div class="panel-footer" align="center">
-              <button type="submit" value="Enviar" class="btn btn-primary">Enviar</button>
-              <button type="reset" value="Cancelar" class="btn btn-primary">Cancelar</button>
+                <button type="submit" value="Enviar" class="btn btn-success">Enviar</button>
+                <button type="reset" value="Cancelar" class="btn btn-warning">Cancelar</button>
             </div>
+        </div>
     </form>
 @endsection
